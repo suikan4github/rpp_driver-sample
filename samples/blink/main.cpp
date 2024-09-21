ff15889af69b0e9c38fcb64505806202051c402d
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-09-21 16:28:38 +0900

    Blinking with GpioBasic is OK.

---
+++ samples/blink/main.cpp
@@ -0,0 +1,32 @@
+// #define I2S_DEBUG
+
+// Include local definition
+#include "gpiobasic.hpp"
+#include "pico/binary_info.h"
+#include "sdkwrapper.hpp"
+
+int main() {
+  // Constants declaration.
+  const uint kLedPin = PICO_DEFAULT_LED_PIN;
+  ::pico_driver::SdkWrapper sdk;
+  ::pico_driver::GpioBasic led(sdk, kLedPin);
+
+  // Use RasPi Pico on-board LED.
+  // 1=> Turn on, 0 => Turn pff.
+  led.SetDir(true);
+
+  // Audio talk thorough
+  while (true) {
+    led.Put(true);  // turn on
+    sdk.sleep_ms(125);
+    led.Put(false);  // turn off
+    sdk.sleep_ms(875);
+  }
+
+  // Information for the picotool.
+  // clang-format off
+  bi_decl(bi_program_description("Blinking LED with GpioBasic class."));
+  bi_decl(bi_program_url("https://github.com/suikan4github/pico_driver-sample"));
+  bi_decl(bi_1pin_with_name(kLedPin, "LED"));
+  // clang-format on
+}
