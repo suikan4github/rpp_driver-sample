7051b68b588ca7b990e28ecff4087f53b19a8ef3
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-09-27 08:18:32 +0900

    Refine the comment.

--- samples/blink/main.cpp
+++ samples/blink/main.cpp
@@ -9,13 +9,14 @@ int main() {
   // Constants declaration.
   const uint kLedPin = PICO_DEFAULT_LED_PIN;
   ::rpp_driver::SdkWrapper sdk;
-  ::rpp_driver::GpioBasic led(sdk, kLedPin);
+  ::rpp_driver::GpioBasic led(sdk,       // SDK dependency injection.
+                              kLedPin);  // GPIO pin # for LED.
 
   // Use RasPi Pico on-board LED.
   // 1=> Turn on, 0 => Turn pff.
   led.SetDir(true);
 
-  // Audio talk thorough
+  // Blinking loop.
   while (true) {
     sdk.sleep_ms(250);
     led.Toggle();
